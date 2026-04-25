26dc279be785747046d4adbef96ce4865f0d3a8a
ashleygomezali32-ux <ashleygomezali32@gmail.com>
2026-04-24 22:43:17 -0400

    Update Actualizador.de.datos.cpp

--- Actualizador.de.datos.cpp
+++ Actualizador.de.datos.cpp
@@ -3,8 +3,20 @@
 
 using namespace std;
 
+MYSQL* conn;
 int id;
+    void conectar() {
+    conn = mysql_init(0);
 
+    conn = mysql_real_connect(conn, "localhost", "root", "TEAMOAKIVA369", "nexus_academy", 3306, NULL, 0);
+
+    if (conn) {
+        cout << "CONEXION EXITOSA" << endl;
+    } else {
+        cout << "ERROR DE MYSQL" << mysql_error(conn) << endl;
+    }
+}
+// Actualizar nota
 void nota()
 {
     float nuevanota;
@@ -15,7 +27,7 @@ void nota()
     cout << "Ingrese nueva nota" << endl;
     cin >> nuevanota;
 
-    string query = "UPDATE -basededatos- SET -nota- =" + string(nuevanota) + "WHERE id = " + to_string(id);
+    string query = " UPDATE alumnos SET nota_final = " + to_string(nuevanota) + " WHERE id = " + to_string(id);
 
     if (msyql_query(conn, query.c_str()) == 0)
     {
@@ -27,7 +39,7 @@ void nota()
     }
     return 0;
 }
-
+// Actualizar celular
 void celular()
 {
     int celular;
@@ -37,7 +49,7 @@ void celular()
     cout << "Ingrese nuevo celular" << endl;
     cin >> celular;
 
-    string query = "UPDATE -basededatos- SET -celular- =" + celular + " WHERE id = " + to_string(id);
+    string query = " UPDATE alumnos SET celular = " + to_string(cel) + " WHERE id = " + to_string(id);
 
     if (msyql_query(conn, query.c_str()) == 0)
     {
@@ -47,5 +59,25 @@ void celular()
     {
         cout << "Error al actualizar celular";
     }
+    int main() {
+    conectar();
+        if (conn != NULL) {
+        int opcion;
+
+        cout << "1. Actualizar nota 2. Actualizar celular";
+        cout << "Elija opcion: ";
+        cin >> opcion;
+
+        if (opcion == 1) {
+            nota();
+        } else if (opcion == 2) {
+            celular();
+        } else {
+            cout << "Opcion invalida";
+        }
+
+        mysql_close(conn);
+    }
+    
     return 0;
 }
