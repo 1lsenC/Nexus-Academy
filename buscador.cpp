10c356d55a401567e00816d7f51909ceb378b8ae
ashleygomezali32-ux <ashleygomezali32@gmail.com>
2026-04-25 22:19:52 -0400

    Update buscador.cpp
    Codigo con comando para conectarse a MySQL

--- buscador.cpp
+++ buscador.cpp
@@ -24,29 +24,26 @@ void buscarAlumno() {
     string consulta = "SELECT * FROM alumnos WHERE id = " + to_string(id);
 
     if (mysql_query(conn, consulta.c_str())) {
-        cout << "Error en la consulta: " << mysql_error(conn) << endl;
+        cout << "Error" << mysql_error(conn) << endl;
         return;
     }
 
     MYSQL_RES *resultado = mysql_store_result(conn);
-    MYSQL_ROW fila;
-
-    fila = mysql_fetch_row(resultado);
+    MYSQL_ROW fila = mysql_fetch_row(resultado);
 
     if (fila != NULL) {
-        cout << "--- DATOS DEL ALUMNO ---"<< endl;
+        cout << "DATOS DEL ALUMNO"<< endl;
         cout << "ID: " << fila[0] << endl;
         cout << "Nombre: " << fila[1] << " " << fila[2] << endl;
         cout << "Celular: " << fila[3] << endl;
         cout << "Nota Final: " << fila[4] << endl;
-    } else {
-        cout << "Alumno no encontrado";
     }
-
+    else { 
+        cout << "Error"<< endl;
+    }
     mysql_free_result(resultado);
 }
-
-int main() {
+int main() { 
     conectar();
 
     if (conn != NULL) {
@@ -56,3 +53,5 @@ int main() {
 
     return 0;
 }
+
+    
